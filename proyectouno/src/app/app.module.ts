0d5ed1ffec012a8f7421c922ae975293c8185c27
Nicolas <nicosms24@gmail.com>
2022-05-19 21:37:35 -0300

    final
    pruebaFinal

--- proyectouno/src/app/app.module.ts
+++ proyectouno/src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoapComponent } from './componentes/logoap/logoap.component';
+import { SocialComponent } from './componentes/social/social.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+/*La linea de abajo permite incluir el circulo de progresion en el proyecto*/
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    SocialComponent,
+    AcercadeComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    /*La linea de abajo permite incluir el circulo de progresion en el proyecto*/
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
